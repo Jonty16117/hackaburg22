cf861dbdab51a7e01431444373257eac5f5ab3cf
Mariem Khlifi <mariemkhlifi16@gmail.com>
2022-07-08 16:44:56 +0200

    sth

--- backend/admin_panel/models.py
+++ backend/admin_panel/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30)
--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,15 +1,50 @@
-# import viewsets
-from rest_framework import viewsets
+from django.http import JsonResponse
+from .serializers import UsersSerializer#, FoodsSerializer, RoomsSerializer, ParkingsSerializer, OfficeBookingsSerializer
+from .models import UsersModel#, FoodsModel, RoomsModel, ParkingsModel, OfficeBookingsModel
+# # import viewsets
+# from rest_framework import viewsets
+
+def listusers(request):
+    users = UsersModel.objects.all()
+    serializer = UsersSerializer(users, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
  
-# import local data
-from .serializers import ApiSerializer
-# from .models import GeeksModel
+# # import local data
  
-# create a viewset
-class ApiViewSet(viewsets.ModelViewSet):
-    # define queryset
-    # queryset = GeeksModel.objects.all()
+# # create a viewset
+# class UsersViewSet(viewsets.ModelViewSet):
+#     # define queryset
+#     queryset = UsersModel.objects.all()
      
-    # specify serializer to be used
-    # serializer_class = GeeksSerializer
-    pass
+#     # specify serializer to be used
+#     serializer_class = UsersSerializer
+
+# class FoodsViewSet(viewsets.ModelViewSet):
+#     # define queryset
+#     queryset = FoodsModel.objects.all()
+     
+#     # specify serializer to be used
+#     serializer_class = FoodsSerializer
+
+# class RoomsViewSet(viewsets.ModelViewSet):
+#     # define queryset
+#     queryset = RoomsModel.objects.all()
+     
+#     # specify serializer to be used
+#     serializer_class = RoomsSerializer
+
+# class ParkingsViewSet(viewsets.ModelViewSet):
+#     # define queryset
+#     queryset = ParkingsModel.objects.all()
+     
+#     # specify serializer to be used
+#     serializer_class = ParkingsSerializer
+
+# class OfficeBookingsViewSet(viewsets.ModelViewSet):
+#     # define queryset
+#     queryset = OfficeBookingsModel.objects.all()
+     
+#     # specify serializer to be used
+#     serializer_class = OfficeBookingsSerializer
