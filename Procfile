c44096c61806b89bc737c0ca604f3d5883343deb
jonty <jontykantiwal@gmail.com>
2022-07-08 19:29:05 +0200

    jas-build failed troubleshooting

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn backend.wsgi
+web: gunicorn hackaburg22.wsgi
--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
-    'backend.herokuapp.com'
+    'hackaburg22.herokuapp.com'
 ]
 
 
