ce5e2e4f9cb8a087ff138ea1e292d6a2f59bb842
jonty <jontykantiwal@gmail.com>
2022-07-08 19:58:30 +0200

    jas-added new apis

--- apis/urls.py
+++ apis/urls.py
@@ -2,11 +2,14 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from apis.views import UsersViewSet
+from apis.views import UsersViewSet, FoodsViewSet, RoomsViewSet, ParkingsViewSet, OfficeBookingsViewSet, RoomAllocatorViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet)
-# router.register(r'species', SpeciesViewSet)
+router.register(r'rooms', RoomsViewSet)
+router.register(r'parkings', ParkingsViewSet)
+router.register(r'foods', FoodsViewSet)
+router.register(r'officebookings', OfficeBookingsViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
